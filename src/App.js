54965f0fdc15a0cdc77015025eaca656a1e175dc
daferarte <daferarte@gmail.com>
2023-04-06 19:14:22 -0500

    Definicion de rutas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-let Hello = () => {
-  return <h1>Hola mundo</h1>
+let NotImplemented = () => {
+  return <h1>Esta pagina aun no esta lista</h1>
 }
 
 function App() {
@@ -10,7 +10,19 @@ function App() {
     <BrowserRouter>
       {/* Routes se usa para generar grupas de rutas  */}
       <Routes>
-        <Route path='/' element={<Hello/>} />
+        <Route path='/' element={<NotImplemented/>} />
+        <Route path='/usuarios/registro' element={<NotImplemented/>} />
+        <Route path='/usuarios/login' element={<NotImplemented/>} />
+
+        <Route path='/usuarios/:id' element={<NotImplemented/>} />
+        <Route path='/usuarios/:id/videos' element={<NotImplemented/>} />
+
+        
+      </Routes>
+      <Routes>
+        <Route path='/videos' element={<NotImplemented/>} />
+        <Route path='/videos/nuevo' element={<NotImplemented/>} />
+        <Route path='/videos/:id' element={<NotImplemented/>} />
       </Routes>
     </BrowserRouter>
   );
