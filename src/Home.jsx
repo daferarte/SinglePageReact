15fb0e0a47645bfc18a380420f49e2b05d1d25e3
daferarte <daferarte@gmail.com>
2023-04-08 19:39:03 -0500

    Trabajo de props style

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,13 +3,27 @@ import styled from 'styled-components';
 
 // Tagget templates
 let Example = styled.div`
-    background-color: red;
     height: 200px;
+    transition: all 0.2s;
+`;
+
+let Button = styled.button`
+    border: solid 1px red;
+    background-color: ${({primary}) => primary ? 'red' : 'transparent'} ;
+`;
+
+let InputText = styled.input`
+    border: solid 1px;
+    border-color: ${({withError})=> withError ? 'red':'green'}
 `;
 
 let HomePage = (props) => {
     return(
-        <Example></Example>
+        <Example>
+            <p> Hola mundo</p>
+            <InputText ></InputText>
+            <Button primary>Ejemplo</Button>
+        </Example>
     )
 }
 
